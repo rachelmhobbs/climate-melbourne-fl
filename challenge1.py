14a0855b29402560fd81095040bfaccb033801bc
Rachel Hobbs <rachel@rachels-mbp-5.myfiosgateway.com>
2022-01-20 20:45:59 -0500

    Third commit

--- challenge1.py
+++ challenge1.py
@@ -44,7 +44,7 @@ def temp_over_100(df):
     return count2
 
 def temp_less_32(df):
-    
+
     '''
     Counts the total number of hours when temperature was strictly less than
     32 degrees using dataframe column called 'temp'.
@@ -56,8 +56,36 @@ def temp_less_32(df):
     print('The total number of hours when temperature was less than 32 was', count3)
     return count3
 
-#def temp_total_ave(df):
+def temp_ave(df):
 
+    '''
+    The mean of the dataframe column called 'temp'.
+
+    Parameter:
+        df: the dataframe
+    '''
+    temp_mean = df['temp'].mean()
+    print('The total average temperature was', temp_mean)
+    return temp_mean
+
+def humidity(df):
+    #got overflow warning, can suppress with warning package
+    #something is wrong with H column - everything is 0? Does this make sense?
+    #also, why are there so little values in H compared to the number of values
+    #in dewpt or temp??
+    '''
+    Adds a new column 'H' to the end of the dataframe that contains humidity
+    values based on information from columns 'dewpt' and 'temp'. Returns the
+    dataframe with the newly added humidity column.
+
+    Parameter:
+        df: the dataframe
+    '''
+    numr = (np.exp(17.625 * df['dewpt']))/(243.04 + df['dewpt'])
+    denom = (np.exp(17.625 * df['temp']))/(243.04 + df['temp'])
+    df['H'] = 100 * (numr/denom)
+    print(df.describe())
+    return df
 
 #def all_calculations(climate_df):
 
@@ -69,10 +97,10 @@ mlb_hrly = load_data(path, filename_mlb) #melbourne dataframe
 sd_hrly = load_data(path, filename_sd) #san diego dataframe
 
 print(mlb_hrly.head())
-#print(mlb_hrly.tail())
+print(mlb_hrly.tail())
 
 print(sd_hrly.head())
-#print(sd_hrly.tail())
+print(sd_hrly.tail())
 
 mlb_rain = total_hrs_rained(mlb_hrly)
 print(mlb_rain)
@@ -92,5 +120,27 @@ print(mlb_temp_ls_32)
 sd_temp_ls_32 = temp_less_32(sd_hrly)
 print(sd_temp_ls_32)
 
+mlb_ave = temp_ave(mlb_hrly)
+print(mlb_ave)
+
+sd_ave = temp_ave(sd_hrly)
+print(sd_ave)
+
+mlb_hrly['time'] = pd.to_datetime(mlb_hrly['time'])
+
+print(mlb_hrly['time']) #it's now a datetime type
+#print(mlb_hrly['time'].min())
+#print(mlb_hrly['time'].max())
+
+mlb_hrly.set_index('time', inplace=True)
+#print(mlb_hrly) #success
+
+#print(mlb_hrly.loc['2019-01']) #values for january
+print(mlb_hrly.loc['2019-01']['temp'].mean()) #january mean
+
 #print(mlb_hrly.describe())
 #print(sd_hrly.describe())
+
+#humidity(mlb_hrly)
+#humidity(sd_hrly)
+#print(mlb_hrly.describe())
